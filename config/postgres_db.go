0cbe4caaef407c51f15c606f3d0777d46b5367a0
erdedani <erdeni1313@gmail.com>
2026-03-24 15:08:58 +0800

    fix env validate _yml

--- config/postgres_db.go
+++ config/postgres_db.go
@@ -2,8 +2,8 @@ package config
 
 type PostgresDB struct {
 	Password string `env:"DB_PASSWORD" validate:"required"`
-	User     string `env:"DB_USER" "validate:"required"`
-	Host     string `env:"DB_HOST" "validate:"required"`
-	Port     string `env:"DB_PORT" "validate:"required"`
-	Name     string `env:"DB_NAME" "validate:"required"`
+	User     string `env:"DB_USER" validate:"required"`
+	Host     string `env:"DB_HOST" validate:"required"`
+	Port     string `env:"DB_PORT" validate:"required"`
+	Name     string `env:"DB_NAME" validate:"required"`
 }
